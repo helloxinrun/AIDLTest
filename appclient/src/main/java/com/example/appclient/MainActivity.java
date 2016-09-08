ce11d0e52236162f4e21b3a50726eec92cdba372
xinrun <2212298003@qq.com>
2016-09-08 18:13:50 +0800

    Adjust the interface

--- appclient/src/main/java/com/example/appclient/MainActivity.java
+++ appclient/src/main/java/com/example/appclient/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
